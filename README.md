48518eb430b3c471b290fd9fe69d79885fb0673b
yiomar <yruiz@solera.pe>
2021-03-16 16:16:04 -0500

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# js-portfolio
+Babel Loader para JavaScript
+<h4>Apuntes</h4>
+Babel te permite hacer que tu código JavaScript sea compatible con todos los navegadores
+Debes agregar a tu proyecto las siguientes dependencias
+
+  NPM
+  npm install -D babel-loader @babel/core @babel/preset-env @babel/plugin-transform-runtime
+
+  Yarn
+  yarn add -D babel-loader @babel/core @babel/preset-env @babel/plugin-transform-runtime
+
+babel-loader nos permite usar babel con webpack
+@babel/core es babel en general
+@babel/preset-env trae y te permite usar las ultimas características de JavaScript
+@babel/plugin-transform-runtime te permite trabajar con todo el tema de asincronismo como ser async y await
+Debes crear el archivo de configuración de babel el cual tiene como nombre .babelrc
+
+  {
+    "presets": [
+      "@babel/preset-env"
+    ],
+    "plugins": [
+      "@babel/plugin-transform-runtime"
+    ]
+  }
+
+Para comenzar a utilizar webpack debemos agregar la siguiente configuración en webpack.config.js
+
+  {
+  ...,
+  module: {
+      rules: [
+        {
+          // Test declara que extensión de archivos aplicara el loader
+          test: /\.js$/,
+          // Use es un arreglo u objeto donde dices que loader aplicaras
+          use: {
+            loader: "babel-loader"
+          },
+          // Exclude permite omitir archivos o carpetas especificas
+          exclude: /node_modules/
+        }
+      ]
+    }
+  }
+
+RESUMEN: Babel te ayuda a transpilar el código JavaScript, a un resultado el cual todos los navegadores lo puedan entender y ejecutar. Trae “extensiones” o plugins las cuales nos permiten tener características más allá del JavaScript común
---
+++ webpack.config.dev.js
@@ -0,0 +1,125 @@
+/** Para trabajar con archivos y rutas de directorios */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  /** Le pasamos explicitamente el modo desde el archivo */
+  mode: 'development',
+
+  watch: true,
+
+  /** El punto de entrada de mi aplicación */
+  entry: './src/index.js',
+
+  /** Esta es la salida de mi bundle */
+  output: {
+    /** Resolve lo que hace es darnos la ruta absoluta de el S.O hasta nuestro archivo,
+     *  para no tener conflictos entre Linux, Windows, etc
+     */
+    path: path.resolve(__dirname, 'dist'),
+
+    /** El Nombre del archivo final */
+    filename: '[name].[contenthash].js',
+
+    assetModuleFilename: 'assets/images/[hash][ext][query]'
+  },
+
+  resolve: {
+    /** Los Archivos que webpack va a leer */
+    extensions: ['.js'],
+    alias: {
+      '@utils': path.resolve(__dirname, 'src/utils/'),
+      '@templates': path.resolve(__dirname, 'src/templates/'),
+      '@styles': path.resolve(__dirname, 'src/styles/'),
+      '@images': path.resolve(__dirname, 'src/assets/images/'),
+    }
+  },
+
+  module: {
+    rules: [
+      {
+        /** Test declara que extensión de archivos aplicara el loader */
+        test: /\.m?js$/,
+
+        /** Use es un arreglo u objeto donde dices que loader aplicaras */
+        use: {
+          loader: "babel-loader",
+        },
+
+        /** Exclude permite omitir archivos o carpetas especificas */
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css|.styl$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'stylus-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2)$/, // REGLA PARA ARCHIVOS WOFF | WOFF2
+        use: {
+          loader: 'url-loader', // NOMBRE DEL LOADER
+          options: {
+            limit: false, // O LE PASAMOS UN NUMERO
+            // Habilita o deshabilita la transformación de archivos en base64.
+            mimetype: 'aplication/font-woff',
+            // Especifica el tipo MIME con el que se alineará el archivo. 
+            // Los MIME Types (Multipurpose Internet Mail Extensions)
+            // son la manera standard de mandar contenido a través de la red.
+            name: "[name].[contenthash].[ext]",
+            // EL NOMBRE INICIAL DEL PROYECTO + SU EXTENSIÓN
+            // PUEDES AGREGARLE [name]hola.[ext] y el output del archivo seria 
+            // ubuntu-regularhola.woff
+            outputPath: './assets/fonts/',
+            // EL DIRECTORIO DE SALIDA (SIN COMPLICACIONES)
+            publicPath: '../assets/fonts/',
+            // EL DIRECTORIO PUBLICO (SIN COMPLICACIONES)
+            esModule: false
+          }
+        }
+      }
+    ]
+  },
+
+  plugins: [
+    /** Configuración del plugin */
+    new HtmlWebpackPlugin({
+      /** Inyecta el bundle al template html */
+      inject: true,
+
+      /** La ruta al template html */
+      template: './public/index.html',
+
+      /** Nombre final del archivo */
+      filename: './index.html',
+    }),
+
+    /** Instanciamos el plugin */
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].[contenthash].css'
+    }),
+
+    // CONFIGURACIÓN DEL COPY PLUGIN
+    new CopyPlugin({
+      patterns: [
+        {
+          // CARPETA A MOVER AL DIST
+          from: path.resolve(__dirname, "src", 'assets/images'),
+          // RUTA FINAL DEL DIST
+          to: "assets/images" 
+        }
+      ]
+    }),
+    new Dotenv(),
+  ],
+};
+
+/**
+ * COMANDOS
+ * npx webpack --mode production --config webpack.config.js
+ */
